37c604807f96e7accf92d1a4ada76f45570a88b7
wanglong <wanglong@ishangzu.com>
2018-04-10 17:00:12 +0800

    springboot整合dubbo

--- web/src/main/java/com/fkxywl/web/WebApplication.java
+++ web/src/main/java/com/fkxywl/web/WebApplication.java
@@ -1,12 +1,16 @@
 package com.fkxywl.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApplication {
+	private static Logger logger = LoggerFactory.getLogger(WebApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+		logger.info("web服务启动成功");
 	}
 }
--- web/src/main/java/com/fkxywl/web/controller/HelloController.java
+++ web/src/main/java/com/fkxywl/web/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.fkxywl.web.controller;
 
+import com.fkxywl.facade.api.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    HelloService helloService;
+
     @RequestMapping(value="/{user}", method= RequestMethod.GET)
     public String hello(@PathVariable String user){
-        return user;
+        return helloService.test().toString();
     }
 }
